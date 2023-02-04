b51ac7a78508a8ce454c34e431417db4682a4e2e
Jabed Khan <mahfuzislam1410@gmail.com>
2023-02-05 02:16:58 +0600

    mysql connection

--- library.py
+++ library.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import mysql.connector
 
+
 class LibraryManagement:
     def __init__(self, root):
         self.root = root
@@ -9,6 +10,24 @@ class LibraryManagement:
         self.root.geometry("1365x767+0+0")
 
         # ======================= Variable ======================
+        self.member_var = StringVar()
+        self.prn_var = StringVar()
+        self.id_var = StringVar()
+        self.firstname_var = StringVar()
+        self.lastname_var = StringVar()
+        self.address1_var = StringVar()
+        self.address2_var = StringVar()
+        self.postcode_var = StringVar()
+        self.mobile_var = StringVar()
+        self.bookid_var = StringVar()
+        self.booktitle_var = StringVar()
+        self.author_var = StringVar()
+        self.dateborrowed_var = StringVar()
+        self.datedue_var = StringVar()
+        self.daysonbook_var = StringVar()
+        self.latereturnfine_var = StringVar()
+        self.dateoverdue_var = StringVar()
+        self.actualprice_var = StringVar()
 
         labeltitle = Label(self.root, text="LIBRARY MANAGEMENT SYSTEM", bg="powder blue", fg="green", bd=10,
                            relief=RIDGE, font=("times new roman", 30, "bold"), padx=2, pady=6)
@@ -30,7 +49,8 @@ class LibraryManagement:
                             padx=2, pady=6)
         labelMember.grid(row=0, column=0, sticky=W)
 
-        ComMember = ttk.Combobox(DataFrameLeft, font=("times new roman", 12, "bold"), width=22, state="readonly")
+        ComMember = ttk.Combobox(DataFrameLeft, textvariable=self.member_var, font=("times new roman", 12, "bold"),
+                                 width=22, state="readonly")
         ComMember["value"] = ("Admin Staff", "Student", "Lecturer", "Professor", "Guest")
         ComMember.grid(row=0, column=1)
 
@@ -39,49 +59,49 @@ class LibraryManagement:
                             padx=2, pady=0)
         labelPRN_No.grid(row=1, column=0, sticky=W)
 
-        txtPRN_No = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtPRN_No = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.prn_var, width=25)
         txtPRN_No.grid(row=1, column=1)
 
         labeltitle = Label(DataFrameLeft, text="Identity No", bg="powder blue", font=("times new roman", 12, "bold"),
                            padx=2, pady=4)
         labeltitle.grid(row=2, column=0, sticky=W)
-        txttitle = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txttitle = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.id_var, width=25)
         txttitle.grid(row=2, column=1)
 
         labelfname = Label(DataFrameLeft, text="First Name", bg="powder blue", font=("times new roman", 12, "bold"),
                            padx=2, pady=4)
         labelfname.grid(row=3, column=0, sticky=W)
-        txtfname = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtfname = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.firstname_var, width=25)
         txtfname.grid(row=3, column=1)
         labellname = Label(DataFrameLeft, text="Last Name", bg="powder blue", font=("times new roman", 12, "bold"),
                            padx=2, pady=4)
         labellname.grid(row=4, column=0, sticky=W)
-        txtlname = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtlname = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.lastname_var, width=25)
         txtlname.grid(row=4, column=1)
 
         labeladdress1 = Label(DataFrameLeft, text="Address 1", bg="powder blue", font=("times new roman", 12, "bold"),
                               padx=2, pady=4)
         labeladdress1.grid(row=5, column=0, sticky=W)
-        txtaddress1 = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtaddress1 = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.address1_var, width=25)
         txtaddress1.grid(row=5, column=1)
 
         labeladdress2 = Label(DataFrameLeft, text="Address 2", bg="powder blue", font=("times new roman", 12, "bold"),
                               padx=2, pady=4)
         labeladdress2.grid(row=6, column=0, sticky=W)
-        txtaddress2 = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtaddress2 = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.address2_var, width=25)
         txtaddress2.grid(row=6, column=1)
 
         labelpostalcode = Label(DataFrameLeft, text="Postal Code", bg="powder blue",
                                 font=("times new roman", 12, "bold"),
                                 padx=2, pady=4)
         labelpostalcode.grid(row=7, column=0, sticky=W)
-        txtpostalcode = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtpostalcode = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.postcode_var, width=25)
         txtpostalcode.grid(row=7, column=1)
 
         labelmobile = Label(DataFrameLeft, text="Mobile", bg="powder blue", font=("times new roman", 12, "bold"),
                             padx=2, pady=4)
         labelmobile.grid(row=8, column=0, sticky=W)
-        txtmobile = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtmobile = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.mobile_var, width=25)
         txtmobile.grid(row=8, column=1)
 
         # ================New Column================
@@ -89,62 +109,64 @@ class LibraryManagement:
         labelbookid = Label(DataFrameLeft, text="Book Id", bg="powder blue", font=("times new roman", 12, "bold"),
                             padx=2, pady=4)
         labelbookid.grid(row=0, column=4, sticky=W)
-        txtbookid = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtbookid = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.bookid_var, width=25)
         txtbookid.grid(row=0, column=5)
 
         labelbookname = Label(DataFrameLeft, text="Book Name", bg="powder blue", font=("times new roman", 12, "bold"),
                               padx=2, pady=4)
         labelbookname.grid(row=1, column=4, sticky=W)
-        txtname = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtname = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.booktitle_var, width=25)
         txtname.grid(row=1, column=5)
 
         labelauthorname = Label(DataFrameLeft, text="Author Name", bg="powder blue",
                                 font=("times new roman", 12, "bold"),
                                 padx=2, pady=4)
         labelauthorname.grid(row=2, column=4, sticky=W)
-        txtauthor = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtauthor = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.author_var, width=25)
         txtauthor.grid(row=2, column=5)
 
         labeldateborrowed = Label(DataFrameLeft, text="Date Borrowed", bg="powder blue",
                                   font=("times new roman", 12, "bold"),
                                   padx=2, pady=4)
         labeldateborrowed.grid(row=3, column=4, sticky=W)
-        txtdateborrowed = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtdateborrowed = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.dateborrowed_var,
+                                width=25)
         txtdateborrowed.grid(row=3, column=5)
 
         labeldatedue = Label(DataFrameLeft, text="Date Due", bg="powder blue",
                              font=("times new roman", 12, "bold"),
                              padx=2, pady=4)
         labeldatedue.grid(row=4, column=4, sticky=W)
-        txtdatedue = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtdatedue = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.datedue_var, width=25)
         txtdatedue.grid(row=4, column=5)
 
         labeldaysonbook = Label(DataFrameLeft, text="Days on Book", bg="powder blue",
                                 font=("times new roman", 12, "bold"),
                                 padx=2, pady=4)
         labeldaysonbook.grid(row=5, column=4, sticky=W)
-        txtdaysonbook = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtdaysonbook = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.daysonbook_var, width=25)
         txtdaysonbook.grid(row=5, column=5)
 
         labellatereturnfine = Label(DataFrameLeft, text="Late Return Fine", bg="powder blue",
                                     font=("times new roman", 12, "bold"),
                                     padx=2, pady=4)
         labellatereturnfine.grid(row=6, column=4, sticky=W)
-        txtlatereturnfine = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtlatereturnfine = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.latereturnfine_var,
+                                  width=25)
         txtlatereturnfine.grid(row=6, column=5)
 
         labeldateoverdue = Label(DataFrameLeft, text="Date Over Due", bg="powder blue",
                                  font=("times new roman", 12, "bold"),
                                  padx=2, pady=4)
         labeldateoverdue.grid(row=7, column=4, sticky=W)
-        txtdateoverdue = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtdateoverdue = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.dateoverdue_var, width=25)
         txtdateoverdue.grid(row=7, column=5)
 
         labelprice = Label(DataFrameLeft, text="Actual Price", bg="powder blue",
                            font=("times new roman", 12, "bold"),
                            padx=2, pady=4)
         labelprice.grid(row=8, column=4, sticky=W)
-        txtprice = Entry(DataFrameLeft, font=("times new roman", 12), width=25)
+        txtprice = Entry(DataFrameLeft, font=("times new roman", 12), textvariable=self.actualprice_var, width=25)
         txtprice.grid(row=8, column=5)
 
         # ============================DataFrame Right Inside============================
@@ -255,135 +277,10 @@ class LibraryManagement:
         self.library_table.column("dateoverdue", width=100)
         self.library_table.column("finalprice", width=100)
 
+    # =================== add data function===============
+
 
 if __name__ == "__main__":
     root = Tk()
     obj = LibraryManagement(root)
     root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
